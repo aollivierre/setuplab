24ac8649d42a354fe6104e19a5b937d32a958970
aollivierre <abdullahollivierre@gmail.com>
2024-08-01 15:35:32 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,10 @@
 # call using:
 
-# powershell -Command "iex (irm https://github.com/aollivierre/setuplab/blob/main/setup.ps1)"
-# powershell -Command "iex (irm https://bit.ly/3WC0w9Y)"
-# powershell -Command "iex (irm bit.ly/3WC0w9Y)"
+# powershell -Command "iex (irm https://raw.githubusercontent.com/aollivierre/setuplab/main/setup.ps1)"
+# powershell -Command "iex (irm https://bit.ly/4c3XH76)"
+# powershell -Command "iex (irm bit.ly/4c3XH76)"
+# or if you are in powershell already call (URL is case sensitive)
+# iex (irm bit.ly/4c3XH76)
 
 # Define a temporary folder path with timestamp
 $timestamp = Get-Date -Format "yyyyMMddHHmmss"
